13fa0499c017bf745f0e8ae892f6b7bc6cf825a5
Maksim Nuzhdin <nettik@Maksims-MacBook-Pro.local>
2026-02-18 14:29:24 +0300

    logging decorator tests

---
+++ internal/task/logging_test.go
@@ -0,0 +1,48 @@
+package task_test
+
+import (
+	"testing"
+
+	"github.com/northmaxota/task-api/internal/domain"
+	"github.com/northmaxota/task-api/internal/task"
+)
+
+type fakeService struct {
+	tasks     task.Task
+	called    bool
+	returnErr error
+}
+
+func (f *fakeService) Create(title string) (task.Task, error) {
+	f.called = true
+	f.tasks = task.Task{Title: title}
+	return f.tasks, f.returnErr
+}
+
+func (f *fakeService) GetAll() ([]task.Task, error) {
+	return nil, nil
+}
+
+func TestLoggingService_Create_Success(t *testing.T) {
+	fake := &fakeService{}
+	loggingSvc := task.NewLoggingService(fake)
+	_, err := loggingSvc.Create("Test Task")
+	if err != nil {
+		t.Errorf("Expected no error, got %v", err)
+	}
+	if !fake.called {
+		t.Error("expected Create to be called on the next service")
+	}
+}
+
+func TestLoggingService_Create_Error(t *testing.T) {
+	fake := &fakeService{returnErr: domain.ErrTitleRequired}
+	loggingSvc := task.NewLoggingService(fake)
+	_, err := loggingSvc.Create("")
+	if err == nil {
+		t.Error("Expected an error, got nil")
+	}
+	if !fake.called {
+		t.Error("expected Create to be called on the next service")
+	}
+}
